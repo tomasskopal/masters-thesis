1bce0a4f821bb5301a65ac701973d24665e5bc91
tomasskopal <tomasskopal01@gmail.com>
2015-09-26 17:08:34 +0200

    first successfuly send message to the server started from the terminal

---
+++ consument/src/main/java/fi/muni/cz/Consumer.java
@@ -0,0 +1,7 @@
+package fi.muni.cz;
+
+/**
+ * Created by tomasskopal on 26.09.15.
+ */
+public class Consumer {
+}
--- producer/src/main/java/fi/muni/cz/DataProducer.java
+++ producer/src/main/java/fi/muni/cz/DataProducer.java
@@ -4,18 +4,20 @@ import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
 
+import java.util.Date;
 import java.util.Properties;
+import java.util.Random;
 
 /**
  * https://cwiki.apache.org/confluence/display/KAFKA/0.8.0+Producer+Example
  */
-public class App {
+public class DataProducer {
 
     public static void main( String[] args ) {
+        Random rnd = new Random();
 
         Properties props = new Properties();
-
-        props.put("metadata.broker.list", "192.168.0.21:9092");
+        props.put("metadata.broker.list", "localhost:9092");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
         props.put("request.required.acks", "1");
 
@@ -23,14 +25,13 @@ public class App {
 
         Producer<String, String> producer = new Producer<>(config);
 
-        //sending...
-        String topic = "data_collect_events";
-        String message = "_Message_1";
-        KeyedMessage<String, String> keyedMessage = new KeyedMessage<>(topic, message);
-        producer.send(keyedMessage);
-
+        for (long nEvents = 0; nEvents < 5; nEvents++) {
+            long runtime = new Date().getTime();
+            String ip = "192.168.2." + rnd.nextInt(255);
+            String msg = runtime + ",www.example.com," + ip;
+            KeyedMessage<String, String> data = new KeyedMessage<>("page_visits", ip, msg);
+            producer.send(data);
+        }
         producer.close();
-
-        System.out.println( "Hello World!" );
     }
 }
