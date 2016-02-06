061016eed58910c1a297784bcef690e27512bd15
tomasskopal <tomasskopal01@gmail.com>
2016-02-06 14:49:58 +0100

    Graylog - configuration for logging into the graylog. v.1

--- app/producer/src/main/java/fi/muni/cz/Application.java
+++ app/producer/src/main/java/fi/muni/cz/Application.java
@@ -1,18 +1,27 @@
 package fi.muni.cz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalTime;
+
 /**
  * Created by tomasskopal on 12.01.16.
  */
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
     /**
      * Just for testing. TODO: delete this class
      */
     public static void main(String[] args) {
-        DataProducer dataProducer = new DataProducer("147.251.43.129", "147.251.43.129", "147.251.43.129");
-        Thread producer = new Thread(
-                dataProducer
-        );
-        producer.start();
+        while (true) {
+            logger.info(LocalTime.now().toString());
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
