1a002a0be3b0f7a73316b563b0dcbd80c6fef5a7
tomasskopal <tomasskopal01@gmail.com>
2016-02-27 11:15:23 +0100

    update rule

--- app/data-store/src/main/java/fi/muni/cz/EpRules.java
+++ app/data-store/src/main/java/fi/muni/cz/EpRules.java
@@ -17,7 +17,7 @@ public class EpRules {
                 + "IncommingEvent(level='LEVEL1').win:time_batch(5 sec) WHERE flag = 'SYN' group by source, level having count(*) > 3");
 
         rules.put("LEVEL2", "select source, count(*) as cnt, level from "
-                + "IncommingEvent(level='LEVEL2').win:time_batch(5 sec) group by source, level having count(*) > 10");
+                + "IncommingEvent(level='LEVEL2').win:time_batch(5 sec) group by source, level having count(*) > 7");
     }
 
     public static EpRules instance() {
